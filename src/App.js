cf954e63dc0c949bd1b44d1516ac371dbcc3a110
Al Cerdan Lico <alcerdan@yahoo.com>
2024-11-09 10:49:18 -0800

    complete testing users interfaces lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import myPhoto from './images/myphoto.jpg'
+
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Al Cerdan Lico</h1>
+      <img src={myPhoto} alt="My Headshot" />
+      <h2>About me</h2>
+      <p>This is a paragraph about my biography</p>
+      <a href='https://www.linkedin.com/in/alcerdanlico/'>LinkedIn</a>
+      <a href='https://github.com/alpogi2dmax/'>Github</a>
+    </div>
+  )
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,4 +3,83 @@ import '@testing-library/jest-dom';
 
 import App from "../App";
 
+
 // Your tests here
+
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+    // Arrange
+    render(<App />);
+  
+    // Act
+    const topLevelHeading = screen.getByRole("heading", {
+      name: /hi, i'm/i,
+      exact: false,
+      level: 1,
+    });
+  
+    // Assert
+    expect(topLevelHeading).toBeInTheDocument();
+  });
+
+  test("displays a second-level heading with the text `About me", () => {
+    // Arrange
+    render(<App />);
+  
+    // Act
+    const secondlevelHeading = screen.getByRole("heading", {
+      name: /about me/i,
+      exact: false,
+      level: 2,
+    });
+  
+    // Assert
+    expect(secondlevelHeading).toBeInTheDocument();
+});
+
+test("displays a image with the alttext `My Headshot", () => {
+    // Arrange
+    render(<App />);
+  
+    // Act
+    const image = screen.getByAltText(/my headshot/i,);
+  
+    // Assert
+    expect(image).toBeInTheDocument();
+});
+
+test("displays a paragraph with the text `biography", () => {
+    // Arrange
+    render(<App />);
+  
+    // Act
+    const biography = screen.getByText(/biography/i,);
+  
+    // Assert
+    expect(biography).toBeInTheDocument();
+});
+
+test("displays a link with the text `Linkedin", () => {
+    // Arrange
+    render(<App />);
+  
+    // Act
+    const linkedinLink = screen.getByRole("link", {
+      name: /linkedin/i,
+    });
+  
+    // Assert
+    expect(linkedinLink).toBeInTheDocument();
+});
+
+test("displays a link with the text `Github", () => {
+    // Arrange
+    render(<App />);
+  
+    // Act
+    const githubLink = screen.getByRole("link", {
+      name: /github/i,
+    });
+  
+    // Assert
+    expect(githubLink).toBeInTheDocument();
+});
